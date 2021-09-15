3e84bdf5c5744b1c5b875706f44370054a9d109e
ducdetronquito <g.paulet@zero-gachis.com>
2021-09-15 13:17:35 +0200

    ⚗️ Tokenize variables

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -11,6 +11,8 @@ pub const TokenType = enum {
     PropertyName, // margin
     PropertyValue, // 0px
     TypeSelector, // h1
+    VariableName, // zig-orange
+    VariableValue, // #f7a41d
 };
 
 pub const Token = struct {
@@ -35,12 +37,11 @@ pub const TokenizerState = enum {
     Done,
     PropertyColonLookup,
     PropertyName,
-    PropertyNameLookup,
     PropertySemicolonLookup,
     PropertyValue,
     PropertyValueLookup,
     SawDot,
-    SawOpenBracket,
+    StartBlock,
     SawSharp,
     Selector,
     SelectorLookup,
@@ -61,93 +62,100 @@ pub const Error = error{
     PropertyValueMustEndWithASemicolon,
     UnexpectedCharacter,
     UnexpectedEndOfFile,
+    VariableNameCanOnlyContainsAlphaChar,
+    VariableNameCannotContainCRLF,
+    VariableValueCannotContainCRLF,
 };
 
 pub const Tokenizer = struct {
-    state: TokenizerState = .Start,
+    current_char: u8 = '\x00',
+    input: []const u8,
     pos: usize = 0,
+    state: TokenizerState = .Start,
     tokenization: Tokenization,
 
-    fn init(allocator: *Allocator) Tokenizer {
-        return .{ .state = .Start, .tokenization = Tokenization.init(allocator) };
+    fn init(allocator: *Allocator, input: []const u8) Tokenizer {
+        return .{ .input = input, .state = .Start, .tokenization = Tokenization.init(allocator) };
     }
 
     fn deinit(self: *Tokenizer) void {
         self.state = .Start;
+        self.current_char = '\x00';
         self.tokenization.deinit();
     }
 
     pub fn tokenize(allocator: *Allocator, input: []const u8) Error!Tokenization {
-        var tokenizer = Tokenizer.init(allocator);
+        var tokenizer = Tokenizer.init(allocator, input);
         errdefer tokenizer.deinit();
 
-        for (input) |char| {
-            try tokenizer.process_char(char);
+        while (tokenizer.pos < tokenizer.input.len) {
+            tokenizer.current_char = tokenizer.input[tokenizer.pos];
+            try tokenizer.next();
         }
 
-        if (tokenizer.state != .Done) {
-            try tokenizer.process_char('\x00');
-        }
+        tokenizer.current_char = '\x00';
+        try tokenizer.next();
 
         return tokenizer.tokenization;
     }
 
-    fn process_char(self: *Tokenizer, char: u8) Error!void {
+    inline fn next(self: *Tokenizer) Error!void {
         var result = switch (self.state) {
             .Done => {},
-            .PropertyColonLookup => self.on_property_colon_lookup(char),
-            .PropertyName => self.on_property_name(char),
-            .PropertyNameLookup => self.on_property_name_lookup(char),
-            .PropertySemicolonLookup => self.on_property_semicolon_lookup(char),
-            .PropertyValue => self.on_property_value(char),
-            .PropertyValueLookup => self.on_property_value_lookup(char),
-            .SawDot => self.on_dot(char),
-            .SawOpenBracket => self.on_open_bracket(char),
-            .SawSharp => self.on_sharp(char),
-            .Selector => self.on_selector(char),
-            .SelectorLookup => self.on_selector_lookup(char),
-            .Start => self.on_start(char),
+            .PropertyColonLookup => self.on_property_colon_lookup(),
+            .PropertyName => self.on_property_name(),
+            .PropertySemicolonLookup => self.on_property_semicolon_lookup(),
+            .PropertyValue => self.on_property_value(),
+            .PropertyValueLookup => self.on_property_value_lookup(),
+            .SawDot => self.on_dot(),
+            .StartBlock => self.on_start_block(),
+            .SawSharp => self.on_sharp(),
+            .Selector => self.on_selector(),
+            .SelectorLookup => self.on_selector_lookup(),
+            .Start => self.on_start(),
         };
         if (result) {
             self.pos += 1;
         } else |err| {
-            std.debug.print("Error={}, State={}, Char: '{}'\n", .{ err, self.state, char });
+            std.debug.print("Error={}, State={}, Char: '{}'\n", .{ err, self.state, self.current_char });
             return err;
         }
     }
 
-    fn on_start(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isSpace(char)) {
-            return;
-        } else if (std.ascii.isAlNum(char)) {
+    fn on_start(self: *Tokenizer) Error!void {
+        self.skipSpace();
+
+        if (isIdentifier(self.current_char)) {
             self.state = .Selector;
             try self.tokenization.tokens.append(Token{ .type = .TypeSelector, .start = self.pos });
             return;
         }
 
-        return switch (char) {
+        return switch (self.current_char) {
             '.' => self.state = .SawDot,
             '#' => self.state = .SawSharp,
+            '$' => {
+                try self.readVariable();
+            },
             '\x00' => self.state = .Done,
             else => error.UnexpectedCharacter,
         };
     }
 
-    fn on_selector_lookup(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isSpace(char)) {
-            return;
-        }
-        if (std.ascii.isAlNum(char)) {
+    fn on_selector_lookup(self: *Tokenizer) Error!void {
+        self.skipSpace();
+
+        if (isIdentifier(self.current_char)) {
             self.state = .Selector;
             try self.tokenization.tokens.append(Token{ .type = .TypeSelector, .start = self.pos });
             return;
         }
 
-        return switch (char) {
+        return switch (self.current_char) {
             '.' => self.state = .SawDot,
             '#' => self.state = .SawSharp,
             '{' => {
-                self.state = .SawOpenBracket;
+                self.state = .StartBlock;
                 try self.tokenization.tokens.append(Token{ .type = .BlockStart, .start = self.pos, .end = self.pos + 1 });
             },
             '\x00' => self.state = .Done,
@@ -155,45 +163,41 @@ pub const Tokenizer = struct {
         };
     }
 
-    fn on_dot(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlpha(char)) {
+    fn on_dot(self: *Tokenizer) Error!void {
+        if (isIdentifier(self.current_char)) {
             try self.tokenization.tokens.append(Token{ .type = .ClassSelector, .start = self.pos });
             self.state = .Selector;
             return;
         }
-        return switch (char) {
+        return switch (self.current_char) {
             '\x00' => error.UnexpectedEndOfFile,
             else => error.ClassSelectorCanOnlyContainsAlphaChar,
         };
     }
 
-    fn on_sharp(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlNum(char)) {
+    fn on_sharp(self: *Tokenizer) Error!void {
+        if (isIdentifier(self.current_char)) {
             try self.tokenization.tokens.append(Token{ .type = .IdSelector, .start = self.pos });
             self.state = .Selector;
             return;
         }
-        return switch (char) {
+        return switch (self.current_char) {
             '\x00' => error.UnexpectedEndOfFile,
             else => error.IdSelectorCanOnlyContainsAlphaChar,
         };
     }
 
-    fn on_selector(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlNum(char)) {
+    fn on_selector(self: *Tokenizer) Error!void {
+        self.readWhile(isIdentifier);
+
+        if (std.ascii.isSpace(self.current_char)) {
+            try self.close_token_and_move_to(.SelectorLookup);
             return;
         }
 
-        if (std.ascii.isSpace(char)) {
-            self.state = .SelectorLookup;
-            try self.set_last_token_end();
-            return;
-        }
-
-        return switch (char) {
+        return switch (self.current_char) {
             '{' => {
-                self.state = .SawOpenBracket;
-                try self.set_last_token_end();
+                try self.close_token_and_move_to(.StartBlock);
                 try self.tokenization.tokens.append(Token{ .type = .BlockStart, .start = self.pos, .end = self.pos + 1 });
             },
             '\x00' => error.UnexpectedEndOfFile,
@@ -201,17 +205,16 @@ pub const Tokenizer = struct {
         };
     }
 
-    fn on_open_bracket(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlpha(char)) {
+    fn on_start_block(self: *Tokenizer) Error!void {
+        self.skipSpace();
+
+        if (isIdentifier(self.current_char)) {
             self.state = .PropertyName;
             try self.tokenization.tokens.append(Token{ .type = .PropertyName, .start = self.pos });
             return;
         }
-        if (std.ascii.isSpace(char)) {
-            self.state = .PropertyNameLookup;
-            return;
-        }
-        return switch (char) {
+        return switch (self.current_char) {
+            '$' => try self.readVariable(),
             '}' => {
                 self.state = .SelectorLookup;
                 try self.tokenization.tokens.append(Token{ .type = .BlockEnd, .start = self.pos, .end = self.pos + 1 });
@@ -221,81 +224,56 @@ pub const Tokenizer = struct {
         };
     }
 
-    fn on_property_name_lookup(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isSpace(char)) {
-            return;
-        }
-        if (std.ascii.isAlpha(char)) {
-            self.state = .PropertyName;
-            try self.tokenization.tokens.append(Token{ .type = .PropertyName, .start = self.pos });
-            return;
-        }
-        return switch (char) {
-            '}' => {
-                self.state = .SelectorLookup;
-                try self.tokenization.tokens.append(Token{ .type = .BlockEnd, .start = self.pos, .end = self.pos + 1 });
-            },
-            '\x00' => error.UnexpectedEndOfFile,
-            else => error.PropertyNameCanOnlyContainsAlphaChar,
-        };
-    }
+    fn on_property_name(self: *Tokenizer) Error!void {
+        self.readWhile(isIdentifier);
 
-    fn on_property_name(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlpha(char) or char == '-') {
+        if (std.ascii.isSpace(self.current_char)) {
+            try self.close_token_and_move_to(.PropertyColonLookup);
             return;
         }
 
-        if (std.ascii.isSpace(char)) {
-            self.state = .PropertyColonLookup;
-            try self.set_last_token_end();
-            return;
-        }
-
-        return switch (char) {
+        return switch (self.current_char) {
             ':' => {
-                self.state = .PropertyValueLookup;
-                try self.set_last_token_end();
+                try self.close_token_and_move_to(.PropertyValueLookup);
             },
             '\x00' => error.UnexpectedEndOfFile,
             else => error.PropertyNameCanOnlyContainsAlphaChar,
         };
     }
 
-    fn on_property_colon_lookup(self: *Tokenizer, char: u8) Error!void {
-        return switch (char) {
-            ' ', '\t' => {},
+    fn on_property_colon_lookup(self: *Tokenizer) Error!void {
+        self.skipBlank();
+        return switch (self.current_char) {
             ':' => self.state = .PropertyValueLookup,
             else => error.UnexpectedCharacter,
         };
     }
 
-    fn on_property_value_lookup(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlNum(char)) {
+    fn on_property_value_lookup(self: *Tokenizer) Error!void {
+        self.skipBlank();
+
+        if (isIdentifier(self.current_char)) {
             self.state = .PropertyValue;
             try self.tokenization.tokens.append(Token{ .type = .PropertyValue, .start = self.pos });
             return;
         }
 
-        return switch (char) {
-            ' ', '\t' => {},
+        return switch (self.current_char) {
             ';' => error.PropertyValueCannotBeEmpty,
             else => error.PropertyValueCanOnlyContainsAlphaChar,
         };
     }
 
-    fn on_property_value(self: *Tokenizer, char: u8) Error!void {
-        if (std.ascii.isAlNum(char)) {
-            return;
-        }
-        return switch (char) {
+    fn on_property_value(self: *Tokenizer) Error!void {
+        self.readWhile(isIdentifier);
+
+        return switch (self.current_char) {
             ';' => {
-                self.state = .PropertyNameLookup;
-                try self.set_last_token_end();
+                try self.close_token_and_move_to(.StartBlock);
                 try self.tokenization.tokens.append(Token{ .type = .EndStatement, .start = self.pos, .end = self.pos + 1 });
             },
             ' ', '\t' => {
-                self.state = .PropertySemicolonLookup;
-                try self.set_last_token_end();
+                try self.close_token_and_move_to(.PropertySemicolonLookup);
             },
             '}' => error.PropertyValueMustEndWithASemicolon,
             '\r', '\n' => error.PropertyValueCannotContainCRLF,
@@ -304,11 +282,11 @@ pub const Tokenizer = struct {
         };
     }
 
-    fn on_property_semicolon_lookup(self: *Tokenizer, char: u8) Error!void {
-        return switch (char) {
-            ' ', '\t' => {},
+    fn on_property_semicolon_lookup(self: *Tokenizer) Error!void {
+        self.skipBlank();
+        return switch (self.current_char) {
             ';' => {
-                self.state = .PropertyNameLookup;
+                self.state = .StartBlock;
                 try self.tokenization.tokens.append(Token{ .type = .EndStatement, .start = self.pos, .end = self.pos + 1 });
             },
             '\r', '\n' => error.NoCRLFBetweenPropertyValueAndSemicolon,
@@ -317,10 +295,85 @@ pub const Tokenizer = struct {
         };
     }
 
-    fn set_last_token_end(self: *Tokenizer) !void {
+    fn readVariable(self: *Tokenizer) Error!void {
+        self.pos += 1;
+        self.current_char = self.input[self.pos];
+        try self.readVariableName();
+        try self.readVariableValue();
+    }
+
+    fn readVariableName(self: *Tokenizer) Error!void {
+        if (!isIdentifier(self.current_char)) {
+            return error.VariableNameCanOnlyContainsAlphaChar;
+        }
+
+        try self.tokenization.tokens.append(Token{ .type = .VariableName, .start = self.pos });
+        self.readWhile(isIdentifier);
+        try self.close_token();
+
+        self.skipBlank();
+
+        return switch (self.current_char) {
+            ':' => self.pos += 1,
+            '\r', '\n' => error.VariableNameCannotContainCRLF,
+            '\x00' => error.UnexpectedEndOfFile,
+            else => error.UnexpectedCharacter,
+        };
+    }
+
+    fn readVariableValue(self: *Tokenizer) Error!void {
+        self.skipBlank();
+
+        try self.tokenization.tokens.append(Token{ .type = .VariableValue, .start = self.pos });
+        self.readWhile(isVariableValue);
+
+        self.skipBlank();
+
+        return switch (self.current_char) {
+            ';' => {
+                try self.close_token();
+                try self.tokenization.tokens.append(Token{ .type = .EndStatement, .start = self.pos, .end = self.pos + 1 });
+            },
+            '\r', '\n' => error.VariableValueCannotContainCRLF,
+            '\x00' => error.UnexpectedEndOfFile,
+            else => error.UnexpectedCharacter,
+        };
+    }
+
+    fn skipBlank(self: *Tokenizer) callconv(.Inline) void {
+        return self.readWhile(std.ascii.isBlank);
+    }
+
+    fn skipSpace(self: *Tokenizer) callconv(.Inline) void {
+        return self.readWhile(std.ascii.isSpace);
+    }
+
+    fn readWhile(self: *Tokenizer, condition: fn (char: u8) bool) callconv(.Inline) void {
+        for (self.input[self.pos..]) |char| {
+            if (!condition(char)) {
+                self.current_char = char;
+                return;
+            }
+            self.pos += 1;
+        }
+    }
+
+    fn isIdentifier(char: u8) bool {
+        return std.ascii.isAlNum(char) or char == '-' or char == '_';
+    }
+
+    fn isVariableValue(char: u8) bool {
+        return char != ';' and char != '\r' and char != '\n' and char != '\x00';
+    }
+
+    fn close_token(self: *Tokenizer) callconv(.Inline) !void {
         var token = self.tokenization.tokens.pop();
         token.end = self.pos;
         try self.tokenization.tokens.append(token);
+    }
+    fn close_token_and_move_to(self: *Tokenizer, state: TokenizerState) callconv(.Inline) !void {
+        try self.close_token();
+        self.state = state;
     }
 };
 
@@ -333,6 +386,20 @@ test "Class selector" {
         Token{ .type = .ClassSelector, .start = 1, .end = 7 },
         Token{ .type = .BlockStart, .start = 7, .end = 8 },
         Token{ .type = .BlockEnd, .start = 8, .end = 9 },
+    };
+
+    try expectTokenEquals(&expected, tokenization.tokens.items);
+}
+
+test "Class selector" {
+    const input = ".big-button{}";
+    var tokenization = try Tokenizer.tokenize(std.testing.allocator, input);
+    defer tokenization.deinit();
+
+    var expected: [3]Token = .{
+        Token{ .type = .ClassSelector, .start = 1, .end = 11 },
+        Token{ .type = .BlockStart, .start = 11, .end = 12 },
+        Token{ .type = .BlockEnd, .start = 12, .end = 13 },
     };
 
     try expectTokenEquals(&expected, tokenization.tokens.items);
@@ -526,6 +593,62 @@ test "Multiple properties in a block" {
         Token{ .type = .BlockEnd, .start = 27, .end = 28 },
     };
 
+    try expectTokenEquals(&expected, tokenization.tokens.items);
+}
+
+test "Variable" {
+    const input = "$zig-orange:#f7a41d;";
+    var tokenization = try Tokenizer.tokenize(std.testing.allocator, input);
+    defer tokenization.deinit();
+
+    var expected: [3]Token = .{
+        Token{ .type = .VariableName, .start = 1, .end = 11 },
+        Token{ .type = .VariableValue, .start = 12, .end = 19 },
+        Token{ .type = .EndStatement, .start = 19, .end = 20 },
+    };
+    try expectTokenEquals(&expected, tokenization.tokens.items);
+}
+
+test "Variable - Space and tabs between variable name and colon are skipped" {
+    const input = "$zig-orange \t : #f7a41d;";
+    var tokenization = try Tokenizer.tokenize(std.testing.allocator, input);
+    defer tokenization.deinit();
+
+    var expected: [3]Token = .{
+        Token{ .type = .VariableName, .start = 1, .end = 11 },
+        Token{ .type = .VariableValue, .start = 16, .end = 23 },
+        Token{ .type = .EndStatement, .start = 23, .end = 24 },
+    };
+    try expectTokenEquals(&expected, tokenization.tokens.items);
+}
+
+test "Variable - Space and tabs between variable value and semicolon are part of the variable value" {
+    const input = "$zig-orange: #f7a41d \t ;";
+    var tokenization = try Tokenizer.tokenize(std.testing.allocator, input);
+    defer tokenization.deinit();
+
+    var expected: [3]Token = .{
+        Token{ .type = .VariableName, .start = 1, .end = 11 },
+        Token{ .type = .VariableValue, .start = 13, .end = 23 },
+        Token{ .type = .EndStatement, .start = 23, .end = 24 },
+    };
+
+    try expectTokenEquals(&expected, tokenization.tokens.items);
+}
+
+test "Variable - Within a block" {
+    const input = ".button{ $zig-orange: #f7a41d;}";
+    var tokenization = try Tokenizer.tokenize(std.testing.allocator, input);
+    defer tokenization.deinit();
+
+    var expected: [6]Token = .{
+        Token{ .type = .ClassSelector, .start = 1, .end = 7 },
+        Token{ .type = .BlockStart, .start = 7, .end = 8 },
+        Token{ .type = .VariableName, .start = 10, .end = 20 },
+        Token{ .type = .VariableValue, .start = 22, .end = 29 },
+        Token{ .type = .EndStatement, .start = 29, .end = 30 },
+        Token{ .type = .BlockEnd, .start = 30, .end = 31 },
+    };
     try expectTokenEquals(&expected, tokenization.tokens.items);
 }
 
