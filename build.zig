c9dbfa1af683f5ce7006ecbb0bea092eaab97255
ducdetronquito <g.paulet@zero-gachis.com>
2021-11-03 09:33:30 +0100

    ✨ Add a minimal CLI

--- build.zig
+++ build.zig
@@ -14,4 +14,19 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const target = b.standardTargetOptions(.{});
+    const exe = b.addExecutable("sasshimi", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
